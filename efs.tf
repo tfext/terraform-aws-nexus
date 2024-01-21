7577a37f721508f8c43b984d99fd3f60c91503a1
Dan Drew <dan.drew@hotmail.com>
2024-01-20 22:42:25 -0800

    Fix variable usage

--- efs.tf
+++ efs.tf
@@ -1,6 +1,6 @@
 data "aws_efs_file_system" "efs" {
   tags = {
-    Name = "corp_cluster_shared_data"
+    Name = var.efs_file_system_name
   }
 }
 
@@ -8,7 +8,7 @@ resource "aws_efs_access_point" "nexus" {
   file_system_id = data.aws_efs_file_system.efs.file_system_id
 
   root_directory {
-    path = "/shared-data/nexus"
+    path = "/shared-data/${local.name}"
     creation_info {
       owner_gid   = 0
       owner_uid   = 0
